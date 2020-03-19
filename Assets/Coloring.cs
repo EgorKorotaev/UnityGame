8c791efd24904e9d1866238dd97a2df0413ebdbe
Faunus_C <FaunusCarmel@yande.ru>
2020-03-20 00:53:58 +0300

    first commit

---
+++ Assets/Coloring.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coloring : MonoBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _renderer.material.color = Color.red;
+            Debug.Log("Setting RED");
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _renderer.material.color = Color.green;
+            Debug.Log("Setting GrEeN");
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _renderer.material.color = Color.blue;
+            Debug.Log("Setting Blue");
+        }
+    }
+}
---
+++ Assets/WolfController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfController : MonoBehaviour
+{
+    [SerializeField] private float _walkSpeed = 1;
+    [SerializeField] private float _runSpeed = 10;
+    [SerializeField] private float rotationSpeed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = _walkSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = _runSpeed;
+        }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
